9e209ecacd6e11cdcd2b174da4d7aff4b9f09cee
Bharatsilavat96 <bharatsilavat96@gmail.com>
2025-12-14 23:37:31 +0530

    Added project

---
+++ BLEApp/BLEApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLEApp
+//
+//  Created by Bharat Shilavat on 14/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
